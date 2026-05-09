447cd15be6c825046ac7b0975ffe489f9de6ed9f
0xHamama <mohdalb07@gmail.com>
2026-05-09 22:40:29 +0400

    Update main.cpp
    Just added the main.cpp code, did not edit anything

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "AttendanceManager.h"
+
+using namespace std;
+
+int main() {
+
+    AttendanceManager manager; // creating an object for AttendanceManager class
+
+    manager.run(); 
+
+    return 0;
+}

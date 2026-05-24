698cf2139d50117762f1000c4f6bec278e114b67
0xHamama <mohdalb07@gmail.com>
2026-05-25 02:03:26 +0400

    added exception handling to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
+
 #include "attendancemanager.h"
+
+#include <exception>
+#include <iostream>
 
 using namespace std;
 
 int main() {
-    AttendanceManager manager;
-    manager.run();
+    try {
+        AttendanceManager manager;
+        manager.run();
+    } catch (const exception& error) {
+        cerr << "Unexpected error: " << error.what() << endl;
+        return 1;
+    } catch (...) {
+        cerr << "Unknown and unexpected error!" << endl;
+        return 1; //program terminated with an error
+    }
 
     return 0;
 }
